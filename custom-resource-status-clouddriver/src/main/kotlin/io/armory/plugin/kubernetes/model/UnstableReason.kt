26e4c4115d2c63d27272031401af337bed3ae91a
shlomodaari <104773977+shlomodaari@users.noreply.github.com>
2023-05-15 11:18:11 -0700

    update(statusresult) empty string && remove addtional unstable check

--- custom-resource-status-clouddriver/src/main/kotlin/io/armory/plugin/kubernetes/model/UnstableReason.kt
+++ custom-resource-status-clouddriver/src/main/kotlin/io/armory/plugin/kubernetes/model/UnstableReason.kt
@@ -1,9 +1,9 @@
 package io.armory.plugin.kubernetes.model
 
 enum class UnstableReason(val message: String) {
-    AVAILABLE_REPLICAS("Waiting for all replicas to be available"),
+//    AVAILABLE_REPLICAS("Waiting for all replicas to be available"),
     OLD_REPLICAS("Waiting for old replicas to finish termination"),
-    READY_REPLICAS("Waiting for all replicas to be ready"),
+//    READY_REPLICAS("Waiting for all replicas to be ready"),
     UPDATED_REPLICAS(
         "Waiting for all replicas to be updated"
     );
